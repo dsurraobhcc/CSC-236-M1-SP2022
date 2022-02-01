91a273aae712af205c2f360e69a914440d408e4c
Dominic Surrao <dsurrao@hotmail.com>
2022-01-31 20:55:04 -0500

    class 2 exercise

--- class2/library-data.sql
+++ class2/library-data.sql
@@ -1,10 +1,26 @@
 use library;
 
 -- let's add some books
-insert into book (id, title)
-values (1, 'Sailing to freedom : maritime dimensions of the Underground Railroad');
+insert into book (title, summary, year_published)
+values 
+	('Sailing to freedom : maritime dimensions of the Underground Railroad',
+		'some summary', 2010),
+	('Black Girl Baking',
+		'another summary', 2000),
+    ('Creatures of the Deep',
+		'new summary', 1999),
+    ('On the Sweet Side',
+		'sweet side summary', 2022),
+	('It\'s a new day',
+		'my summary', 2022);
+        
+insert into book (title, summary, year_published)
+	values ('test title', 'test summary', 2000);
+    
+-- generates error
+-- insert into book (title, summary) values ('test title 2', 'test summary 2');
+-- Error Code: 1364. Field 'year_published' doesn't have a default value
 
-insert into book (id, title)
-values (2, 'Black Girl Baking');
+
 
 -- truncate table book;
--- class2/library-schema.sql
+++ class2/library-schema.sql
@@ -3,7 +3,11 @@ create schema library;
 use library;
 
 create table book (
-	id int auto_increment,
-    title varchar(200)
+	id int primary key auto_increment, -- primary key constraint
+    title varchar(200) not null,
+    summary varchar(3000),
+    year_published year not null -- not null constraint
 );
 
+-- Error Code: 1075. Incorrect table definition; 
+-- there can be only one auto column and it must be defined as a key
